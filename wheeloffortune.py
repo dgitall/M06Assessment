fe080b89f26f0fb8e50db13f108a53a860430972
Darrell Gerber <darrellgerber@gmail.com>
2021-12-23 22:05:41 -0600

    12/23/2021 Late

--- wheeloffortune.py
+++ wheeloffortune.py
@@ -9,17 +9,57 @@ RSLT_STOP = -1
 globalStringRscs = {}
 with open('stringfile.json') as json_file:
     globalStringRscs = json.load(json_file)
+
+# Function to evaluate the loaded f-strings. This is necessary because you can't include
+# dictionary keys if doing it inline due to interference with single and double quotes.
+# Taken from: https://stackoverflow.com/questions/47597831/python-fstring-as-function
+def fstr(fstring_text, locals, globals=None):
+    # Dynamically evaluate the provided fstring_text. Passing in locals and globals allows us
+    # to access variables to insert into the f string.
+    locals = locals or {}
+    globals = globals or {}
+    ret_val = eval(f'f"{fstring_text}"', locals, globals)
+    return ret_val
     
+## Prepare the application and print a greeting
 def ApplicationStartup():
     PuzzleDict = {}
     PlayerList = []
     GameSettings = {}
+    # Greeting
+    print(globalStringRscs['StartBanner'])
     
+    # Load the list of puzzles and clues
+    with open('phrases.json') as json_file:
+        PuzzleDict = json.load(json_file)
+        
+    # Create and initialize the list of players
+    PlayerList = [{'Name': None, 'Bio': None, 'GamesPlayed': 0, 'GamesWon': 0, 'TotalWinnings': 0},
+                  {'Name': None, 'Bio': None, 'GamesPlayed': 0, 'GamesWon': 0, 'TotalWinnings': 0},
+                  {'Name': None, 'Bio': None, 'GamesPlayed': 0, 'GamesWon': 0, 'TotalWinnings': 0}]
+        
+    # Create and Initialize the default game Settings
+    GameSettings = {'MillionCard': False}
+
     return PuzzleDict, PlayerList, GameSettings
 
+## Ask the three players to register
 def ContestantSignin(PlayerList):
     result = RSLT_NONE
     
+    print(globalStringRscs['ContestantSignupBanner'])
+    for ContestantIndex in range(0,3):
+        print(fstr(globalStringRscs['ContestantBanner'], locals()))
+        name = str()
+        while len(name) == 0:
+            name = input(globalStringRscs['ContestantName'])
+        bio = input(globalStringRscs['ContestantBio'])
+        PlayerList[ContestantIndex]['Name'] = name
+        if len(bio)==0:
+            PlayerList[ContestantIndex]['Bio'] = None
+        else:
+            PlayerList[ContestantIndex]['Bio'] = bio
+        
     return result
 
 
@@ -57,7 +97,7 @@ def ExitGame(PlayerList):
 
 ## The Main Application where everything starts and ends
 def MainApplication():
-    result = 0
+    result = RSLT_NONE
     
     PlayerList = list()
     PuzzleDict = dict()
@@ -70,24 +110,22 @@ def MainApplication():
     # Show Main Menu
     print(globalStringRscs['MainMenu'])
     
-    # Get User Input
+    # Get User Input and direct to the function dealing with the selection
     while result != RSLT_STOP:
         UserInput = input(globalStringRscs['MenuPrompt'])
-        UserInput = RSLT_NONE
-        if UserInput == 1:
+        if UserInput == '1':
             pass
             result = PlayGame(PuzzleDict, PlayerList, GameSettings)
-        elif UserInput == 2:
+        elif UserInput == '2':
             pass
             result = DisplayDirections()
-        elif UserInput == 3:
+        elif UserInput == '3':
             pass
             result = ShowPlayerStats(PlayerList)
-        elif UserInput == 4:
+        elif UserInput == '4':
             pass
             result = ChangeSettings(GameSettings)
-        elif UserInput == 5:
-            pass
+        elif UserInput == '5':
             result = ExitGame(PlayerList)
         
     
